9c917a56e41a0a483b2b5cb03efddd5a9c99849e
shizhong <shizhong1989@gmail.com>
2023-04-26 12:53:50 -0700

    Add Phone.java and Smartphone.java

--- src/Phone.java
+++ src/Phone.java
@@ -5,8 +5,13 @@ public abstract class Phone {
     protected boolean isSignalAvailable;
     protected boolean isCallAnswered;
 
-    public Phone(String brand) {
+    public Phone(String brand, String phoneNumber) {
         this.brand = brand;
+        this.phoneNumber = phoneNumber;
+        this.batteryLevel = 100;
+        this.storageLevel = 100;
+        this.isSignalAvailable = true;
+        this.isCallAnswered = false;
     }
 
     public String getBrand() {
@@ -17,7 +22,59 @@ public abstract class Phone {
         this.brand = brand;
     }
 
+    public void call(String phoneNumber) throws InvalidPhoneNumberException, NoSignalException, CallNotAnsweredException {
+        if (!isValidPhoneNumber(phoneNumber)) {
+            throw new InvalidPhoneNumberException("Invalid phone number: " + phoneNumber);
+        }
+        if (!isSignalAvailable) {
+            throw new NoSignalException("No signal available");
+        }
+        System.out.println("Calling " + phoneNumber + "...");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.err.println("Call interrupted: " + e.getMessage());
+        }
+        if (!isCallAnswered) {
+            throw new CallNotAnsweredException("Call not answered");
+        }
+        System.out.println("Call ended");
+    }
+
+    public void answerCall() {
+        isCallAnswered = true;
+        System.out.println("Call answered");
+    }
+
+    public void rechargeBattery(int amount) throws BatteryDrainException {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+        if (batteryLevel + amount > 100) {
+            throw new BatteryDrainException("Battery level cannot exceed 100");
+        }
+        batteryLevel += amount;
+        System.out.println("Battery level: " + batteryLevel);
+    }
+
+    public void useStorage(int amount) throws StorageFullException {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+        if (storageLevel - amount < 0) {
+            throw new StorageFullException("Storage full: cannot store more data");
+        }
+        storageLevel -= amount;
+        System.out.println("Storage level: " + storageLevel);
+    }
+
+    public boolean isValidPhoneNumber(String phoneNumber) {
+        return phoneNumber.matches("\\d{10}");
+    }
+
+
 }
+
 
 class Main {
     public static void main(String[] args) {
--- src/Smartphone.java
+++ src/Smartphone.java
@@ -1,9 +1,20 @@
 class Smartphone extends Phone {
     private String os;
 
-    public Smartphone(String brand, String os) {
-        super(brand);
+    public Smartphone(String brand, String os, String phoneNumber) {
+        super(brand, phoneNumber);
         this.os = os;
+    }
+
+    public void installApp(String appName) throws StorageFullException {
+        useStorage(10); // each app takes up 10 storage units
+        System.out.println("Installed " + appName);
+    }
+
+    public void installApp(String appName) throws BatteryDrainException, StorageFullException {
+        rechargeBattery(5); // installing an app drains 5 battery units
+        useStorage(10); // each app takes up 10 storage units
+        System.out.println("Installed " + appName);
     }
 
     public String getOs() {
